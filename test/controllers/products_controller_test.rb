335eea17073d5b9fe464143cb89287ffa12e4609
s-yoshi210 <y.shimazu210@gmail.com>
2020-11-04 14:51:23 +0900

    update

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -10,11 +10,6 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get new" do
-    get new_product_url
-    assert_response :success
-  end
-
   test "should create product" do
     assert_difference('Product.count') do
       post products_url, params: { product: { name: @product.name, price: @product.price, vendor: @product.vendor } }
